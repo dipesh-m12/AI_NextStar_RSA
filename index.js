0c6db489fd4976f10990d88014fd110eceb9c0d6
dipesh-m12 <mavinash423@gmail.com>
2025-09-16 16:21:29 +0530

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
